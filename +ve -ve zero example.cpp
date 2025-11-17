a264058264ad3e59139aaaec0d32e1a6c147c920
sumithralenka <sumithralenka@gmail.com>
2025-11-17 19:05:53 +0530

    Add files via upload

---
+++ +ve -ve zero example.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("enter any number");
+	scanf("%d",&num);
+	if(num=0)
+	printf("zero");
+	else if(num>0)
+	printf(" it is positive value ");
+	else
+	printf(" it is negetive value");
+	return 0;
+}
