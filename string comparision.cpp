38627b1e8481f973ec33df2e3eb9e94ecfcdb4ae
sumithralenka <sumithralenka@gmail.com>
2025-11-23 15:56:51 +0530

    Add files via upload

---
+++ string comparision.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+
+    char a[100]="likhi";
+    char b[100]="likhi";
+    int x;
+    x=strcmp(b,a);
+    printf("string of comparision %d",x);
+    return 0;
+}
